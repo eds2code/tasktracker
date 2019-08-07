3f543d5d04e25a34b35b60f1ad5c3542ec4120bc
vnurov <vladislavnurov@ya.ru>
2019-08-07 11:25:59 +0300

    show all tasks

--- client/store/timetracker/getters.js
+++ client/store/timetracker/getters.js
@@ -1,7 +1,7 @@
 const todayTimeStamp = new Date().setHours(0, 0, 0, 0);
 
 export default {
-  tasks: state => state.tasks
-    .filter(task => new Date(task.startDateTime).setHours(0, 0, 0, 0) === todayTimeStamp),
+  tasks: state => state.tasks,
+    // .filter(task => new Date(task.startDateTime).setHours(0, 0, 0, 0) === todayTimeStamp),
   reportText: state => state.reportText,
 };
