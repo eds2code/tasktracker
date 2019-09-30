9df5e19b5c7b3ada809b00828abb310557a3d229
Владислав Нуров <vladislavnurov@ya.ru>
2019-10-01 01:07:20 +0300

    ref: app-card.vue

--- client/pages/Home.vue
+++ client/pages/Home.vue
@@ -2,31 +2,14 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <div class="links">
-          <router-link  v-for="link in links"
-                        :to="{ name: link.name }"
-                        :key="link.name"
-                        :class="`link link_${link.name}`"
-
-          >
-            <div class="link__content"
-                 @click="setLastClickedLink(link.name)"
-            >
-              <div :class="`link__icon link__icon_${link.name}`"></div>
-              <div class="link__title">{{ link.title }}</div>
-              <div class="link__descript"
-                   v-if="link.descript"
-              > {{ link.descript }}
-              </div>
-            </div>
-          </router-link>
-
-          <div class="link link_stub">
-            <div class="link__content">?</div>
-          </div>
-          <div class="link link_stub">
-            <div class="link__content">?</div>
-          </div>
+        <div class="apps">
+          <AppCard v-for="app in apps"
+                   :key="app.name"
+                   :app="app"
+                   @click="setLastClickedLink(app.name)"
+          />
+          <AppCard :app="{}" :isStub="true" />
+          <AppCard :app="{}" :isStub="true" />
         </div>
       </div>
     </div>
@@ -35,21 +18,27 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import AppCard from './Home/app_card.vue';
 
 export default {
+  components: { AppCard },
   data() {
     return {
-      lastClickedLink: '',
-      links: [
-        { title: 'Daily план', name: 'timetracker', descript: 'Составляй список задач на день, засекай время, создавай отчет.' },
-        { title: 'Копилка', name: 'moneytracker', descript: 'Планируй бюджет, веди учет расходов, копи деньги.' },
+      lastClickedApp: '',
+      apps: [
+        {
+          title: 'Daily план', name: 'timetracker', descript: 'Составляй список задач на день, засекай время, создавай отчет.', emoji: '⏱',
+        },
+        {
+          title: 'Копилка', name: 'moneytracker', descript: 'Планируй бюджет, веди учет расходов, копи деньги.', emoji: '💸',
+        },
       ],
     };
   },
 
   watch: {
     'currentUser._id': function () {
-      this.$router.push({ name: this.lastClickedLink });
+      this.$router.push({ name: this.lastClickedApp });
     },
   },
 
@@ -60,103 +49,20 @@ export default {
   },
 
   methods: {
-    setLastClickedLink(name = '') {
-      this.lastClickedLink = name;
+    setLastClickedApp(appName = '') {
+      this.lastClickedApp = appName;
     },
   },
 };
 </script>
 
 <style scoped>
-.links {
+.apps {
   display: flex;
   flex-wrap: wrap;
   align-content: flex-start;
   justify-content: flex-start;
   width: auto;
   margin: -10px;
-}
-
-.link {
-  user-select: none;
-  position: relative;
-  display: block;
-  width: calc(25% - 20px);
-  margin: 10px;
-  border: 1px solid #eee;
-  background: #ffdd66;
-  overflow: hidden;
-  border-radius: 10px;
-  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.1);
-}
-.link:after {
-  content: "";
-  display: block;
-  padding-bottom: 100%;
-}
-.link:active,
-.link:hover,
-.link:focus {
-  transform: translateY(-5px);
-  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
-}
-
-.link__content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 100%;
-  height: 100%;
-  padding: 40px;
-}
-
-.link__icon {
-  font-size: 80px;
-  height: 80px;
-  width: 80px;
-  margin-bottom: 20px;
-}
-.link__icon_timetracker::after {
-  content: '⏱️';
-}
-.link__icon_moneytracker::after {
-  content: '💸';
-}
-.link__title {
-  text-decoration: none;
-  color: #444;
-  font-weight: bold;
-  font-size: 24px;
-}
-.link__descript {
-  text-decoration: none;
-  color: #666;
-  font-size: 12px;
-  line-height: 18px;
-  margin-top: 15px;
-  letter-spacing: .1px;
-  text-align: center;
-}
-
-/*  */
-.link_stub {
-  background: #fff;
-  box-shadow: none;
-  color: #ddd;
-  border: 1px dashed #ddd;
-  cursor: default;
-  font-weight: normal;
-}
-.link_stub:active,
-.link_stub:focus,
-.link_stub:hover {
-  transform: translateY(0);
-  box-shadow: none;
 }
 </style>
---
+++ client/pages/Home/app_card.vue
@@ -0,0 +1,120 @@
+<template>
+    <router-link :to="{ name: app.name }"
+                 class="app-card"
+                 :class="isStub ? 'app-card_stub' : 'app-card_' + app.name"
+    >
+      <div class="app-card__content">
+        <template v-if="isStub"> ? </template>
+        <template v-else>
+          <div class="app-card__emoji">{{ app.emoji || '👾' }}</div>
+          <div class="app-card__title">{{ app.title }}</div>
+          <div class="app-card__descript"
+              v-if="app.descript"
+          > {{ app.descript }}
+          </div>
+        </template>
+      </div>
+    </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    app: {
+      type: Object,
+      required: true,
+    },
+    isStub: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.app-card {
+  display: block;
+  width: calc(25% - 20px);
+  margin: 10px;
+  display: block;
+  user-select: none;
+  position: relative;
+  border: 1px solid #eee;
+  background: #ffdd66;
+  overflow: hidden;
+  border-radius: 10px;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.1);
+}
+.app-card:after {
+  content: "";
+  display: block;
+  padding-bottom: 100%;
+}
+.app-card:active,
+.app-card:hover,
+.app-card:focus {
+  transform: translateY(-5px);
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
+}
+
+.app-card__content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  padding: 40px;
+}
+
+.app-card__emoji {
+  font-size: 80px;
+  height: 80px;
+  width: 80px;
+  margin-bottom: 20px;
+}
+
+.app-card__title {
+  text-decoration: none;
+  color: #444;
+  font-weight: bold;
+  font-size: 24px;
+}
+.app-card__descript {
+  text-decoration: none;
+  color: #666;
+  font-size: 12px;
+  line-height: 18px;
+  margin-top: 15px;
+  letter-spacing: .1px;
+  text-align: center;
+}
+
+/*  */
+.app-card_stub {
+  background: #fff;
+  box-shadow: none;
+  color: #ddd;
+  border: 1px dashed #ddd;
+  cursor: default;
+  font-weight: normal;
+}
+.app-card_stub:active,
+.app-card_stub:focus,
+.app-card_stub:hover {
+  transform: translateY(0);
+  box-shadow: none;
+}
+
+/*  */
+.app-card_moneytracker {
+  background: rgb(119, 241, 201);
+}
+</style>
